6307e2b86f3ee39722220f5005af6fc72413ec7f
neelvaidya133 <neelvaidya122@gmail.com>
2024-07-27 04:44:13 -0400

    homelayout-navabr-menubar-profliebutton-themebbutton- added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
     template: "%s | NervesPluse",
-     default: "NervesPluse",
+    default: "NervesPluse",
   },
   description: "Social Media App for everyone, pulsing with nerve",
 };
@@ -19,7 +20,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem={true}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
