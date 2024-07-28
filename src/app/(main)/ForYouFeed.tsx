cd94b8d337c188927ee7573fc0557d634f31c73f
neelvaidya133 <neelvaidya122@gmail.com>
2024-07-28 17:54:50 -0400

    added infinite Scroll with skeloton view in post feed page

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -1,21 +1,49 @@
 "use client";
 
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import Post from "@/components/posts/Post";
+import PostsLoadingSkeloton from "@/components/posts/PostsLoadingSkeloton";
+import { Button } from "@/components/ui/button";
 import kyInstance from "@/lib/ky";
-import { PostData } from "@/lib/types";
-import { useQuery } from "@tanstack/react-query";
+import { PostData, PostsPage } from "@/lib/types";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 
 export default function ForYouFeed() {
-  const query = useQuery<PostData[]>({
+  const {
+    data,
+    fetchNextPage,
+    isFetching,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
     queryKey: ["post-feed", "for-you"],
-    queryFn: kyInstance.get("/api/posts/for-you").json<PostData[]>,
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/posts/for-you",
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
-  if (query.status === "pending") {
-    return <Loader2 className="mx-auto animate-spin" />;
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+  console.log(posts);
+  if (status === "pending") {
+    return <PostsLoadingSkeloton />;
   }
-  if (query.status === "error") {
+
+  if (status === "success" && !posts.length && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">
+        No one has posted yet.
+      </p>
+    );
+  }
+  if (status === "error") {
     return (
       <p className="text-center text-destructive">
         An Error Occured While Loading posts
@@ -23,10 +51,14 @@ export default function ForYouFeed() {
     );
   }
   return (
-    <div className="space-y-5">
-      {query.data.map((post) => (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
-    </div>
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollContainer>
   );
 }
